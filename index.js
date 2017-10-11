38c17297e175fa92651457a1a8d37951945880e7
kieffer <nicolas.kieffer@inist.fr>
2017-10-11 15:51:54 +0200

    Fix path of tpl

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 /* jslint indent: 2 */
 'use strict';
 
-let fs = require('fs');
+let fs = require('fs'),
+  path = require('path');
 
 let resources = {
   "trainings": {},
   "mapping": require("./mapping.json"),
   "verbalization": require("./verbalization.json"),
-  "template": fs.readFileSync("./fragment.tei.xml.tpl", 'utf-8')
+  "template": fs.readFileSync(path.join(__dirname, "fragment.tei.xml.tpl"), 'utf-8')
 };
 
 for (let key in resources.mapping) {
